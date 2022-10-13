0adb1cfc8568a906ed341974fa8574c64fe5cd26
Adhicitta Masran <adhicittamasran@gmail.com>
2022-10-13 21:55:56 +0800

    add status json writer

--- main.go
+++ main.go
@@ -1,5 +1,13 @@
 package main
 
+import (
+	"encoding/json"
+	"io/ioutil"
+	"log"
+	"math/rand"
+	"time"
+)
+
 type Status struct {
 	Water int `json:"water"`
 	Wind  int `json:"wind"`
@@ -9,5 +17,33 @@ type Data struct {
 	Status `json:"status"`
 }
 
+func updateData() {
+	for {
+		var data = Data{Status: Status{}}
+		statusMin := 1
+		statusMax := 30
+
+		data.Status.Water = rand.Intn(statusMax-statusMin) + statusMin
+
+		data.Status.Wind = rand.Intn(statusMax-statusMin) + statusMin
+
+		b, err := json.MarshalIndent(&data, "", "  ")
+
+		if err != nil {
+			log.Fatalln("error while marshalling json data  =>", err.Error())
+		}
+
+		err = ioutil.WriteFile("data.json", b, 0644)
+
+		if err != nil {
+			log.Fatalln("error while writing value to data.json file  =>", err.Error())
+		}
+		time.Sleep(time.Second * 15)
+	}
+}
+
 func main() {
+	rand.Seed(time.Now().UnixNano())
+
+	go updateData()
 }
